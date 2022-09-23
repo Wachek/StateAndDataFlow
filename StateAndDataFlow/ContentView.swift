741f0b1e197f4c5ab1f9a95a83c6e99ed0f2cab7
Wachek <wachek7@gmail.com>
2022-09-23 21:59:03 +0300

    initial commit

--- StateAndDataFlow/ContentView.swift
+++ StateAndDataFlow/ContentView.swift
@@ -8,14 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var tapCount = 0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("\(tapCount)")
+                .font(.largeTitle)
+                .padding(.top, 100)
+            Spacer()
+            ButtonView(tapCount: $tapCount)
+            Spacer()
+        }
+        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct ButtonView: View {
+    
+   @Binding var tapCount: Int
+    
+    var body: some View {
+        Button("Tap Me") {
+            tapCount += 1
+        }
     }
 }
